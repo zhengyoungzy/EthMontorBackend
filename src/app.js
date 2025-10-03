af4bf9788441da64f0c7d50f8d9ce9ea27da6665
Lucian Zheng <zheng.young.zy@gmail.com>
2025-10-04 02:05:22 +1000

    fix

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/latest-block", gasRoutes);
+app.use("/latest-block", gasRoutes);
 // Root route
 app.get("/", (req, res) => {
     res.json({ message: "Ethereum Monitor Backend is running 🚀" });
